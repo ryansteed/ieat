16b838883047b254ea40f2b1f5bc190ae186bb12
Ryan Steed <rsteed11@gmail.com>
2020-09-10 16:52:49 -0400

    :sparkles: Completed OpenAI extraction method & initial tests

--- ieat/api.py
+++ ieat/api.py
@@ -6,7 +6,36 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 import os
+from collections import namedtuple
 import pandas as pd
+
+
+models = {
+	"logit": LogitExtractor(
+		model_type,
+		model_size=model_size,
+		models_dir=models_dir,
+		color_clusters_dir=clusters_dir,
+		n_px=n_px,
+		from_cache=from_cache
+	),
+	"sent": SENTExtractor(
+		model_type,
+		model_size=model_size,
+		models_dir=models_dir,
+		color_clusters_dir=clusters_dir,
+		n_px=n_px,
+		from_cache=from_cache
+	),
+	"openai": OpenAIExtractor(
+		model_type,
+		model_size=model_size,
+		models_dir=models_dir,
+		color_clusters_dir=clusters_dir,
+		n_px=n_px,
+		from_cache=from_cache
+	)
+}
 
 
 def test(
@@ -37,32 +66,6 @@ def test(
 
 	# get the embeddings
 	embeddings = []
-	models = {
-		"logit": LogitExtractor(
-			model_type,
-			model_size=model_size,
-			models_dir=models_dir,
-			color_clusters_dir=clusters_dir,
-			n_px=n_px,
-			from_cache=from_cache
-		),
-		"sent": SENTExtractor(
-			model_type,
-			model_size=model_size,
-			models_dir=models_dir,
-			color_clusters_dir=clusters_dir,
-			n_px=n_px,
-			from_cache=from_cache
-		),
-		"openai": OpenAIExtractor(
-			model_type,
-			model_size=model_size,
-			models_dir=models_dir,
-			color_clusters_dir=clusters_dir,
-			n_px=n_px,
-			from_cache=from_cache
-		)
-	}
 	extractor = models.get(model_type)
 	assert extractor is not None, f"Model type '{model_type}' not found."
 
@@ -75,3 +78,53 @@ def test(
 	logger.info("Running test")
 	test = Test(*embeddings, names=[os.path.basename(d) for d in input_dirs])
 	return test.run(**test_params)
+
+
+def test_all(
+		model_types, model_size, models_dir, clusters_dir, n_px, # model details
+		experiment_path=None,
+		file_types=[".jpg", ".jpeg", ".png", ".webp"],
+		from_cache=True,
+		verbose=True,
+		**test_params
+	):
+	"""
+	Produce a table of model_type x test results.
+	:param model_types: List of models to test.
+	:param model_size: iGPT model size - e.g. 's', 'm', or 'l'
+	:param models_dir: directory of iGPT model checkpoints
+	:param clusters_dir: directory of iGPT color cluster files
+	:param n_px: number of pixels in the input images
+	:param file_types: acceptable image file types
+	:param from_cache: whether to use cached embeddings at the location `embedding_path`
+	:param verbose: whether to print out images, other detailed logging info
+	:param experiment_path: path to organized directory of experiments
+	:return: the test effect size and p-value
+	:return:
+	"""
+	TestData = namedtuple('TestData', ['name', 'X', 'Y', 'A', 'B'])
+	tests = []
+	if os.path.exists(experiment_path):
+		for root, dirs, files in os.walk(experiment_path):
+			for dir in dirs:
+				for experiment, categories, _ in os.walk(dir):
+					tests.append(TestData(experiment, *categories))
+	print(tests)
+
+
+if __name__ == "__main__":
+	# some default settings
+	model_size = "s"
+	models_dir = "models"
+	color_clusters_dir = "clusters"
+	n_px = 32
+
+	test_all(
+		model_type="openai",
+		model_size=model_size,
+		models_dir=models_dir,
+		clusters_dir=color_clusters_dir,
+		n_px=n_px,
+		experiment_path="data/experiments",
+		verbose=True
+	)
--- ieat/models.py
+++ ieat/models.py
@@ -1,28 +1,31 @@
-## Code adapted from https://colab.research.google.com/github/apeguero1/image-gpt/blob/master/Transformers_Image_GPT.ipynb 
-## - thanks to the author
-
 from ieat.utils import resize, normalize_img, color_quantize_np
 
 import os
-os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
 import transformers
-from transformers.modeling_gpt2 import GPT2Model,GPT2LMHeadModel
+from transformers.modeling_gpt2 import GPT2LMHeadModel
 
 import torch
 import torch.nn as nn
 from torch.nn.parameter import Parameter
+import tensorflow as tf
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 import logging
+
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 logger = logging.getLogger()
+
+# # Code adapted from https://colab.research.google.com/github/apeguero1/image-gpt/blob/master/Transformers_Image_GPT
+# .ipynb # - thanks to the author
 
 
 class EmbeddingExtractor:
-	def __init__(self, model_name):
+	def __init__(self, model_name, from_cache):
+		self.from_cache = from_cache
 		self.model_name = model_name
 		self.model = None
 
@@ -38,24 +41,25 @@ class EmbeddingExtractor:
 		if self.from_cache and os.path.exists(embedding_path):
 			logger.info("Loading embeddings for %s from file" % os.path.basename(d))
 			encs = pd.read_csv(embedding_path, index_col=0).set_index("img")
-			if (visualize):
+			if visualize:
 				self.process_samples(image_paths, visualize=True)
 		else:
 			logger.info("Extracting embeddings for %s" % os.path.basename(d))
 			# suppress annoying logger output
 			encs = self.extract(
-				image_paths, 
-				output_path=embedding_path, 
-				visualize=visualize, 
+				image_paths,
+				output_path=embedding_path,
+				visualize=visualize,
 				**extract_params
 			)
 		return encs
 
 	def extract(self, image_paths, output_path=None, gpu=False, visualize=False, **extract_kwargs):
-		if self.model is None: self.load_model()
+		if self.model is None:
+			self.load_model()
 		samples = self.process_samples(image_paths, visualize=visualize)
 
-		with torch.no_grad(): # saves some memory
+		with torch.no_grad():  # saves some memory
 
 			# model specific context extraction
 			encs = pd.DataFrame(self._extract_context(samples, gpu, **extract_kwargs))
@@ -64,7 +68,7 @@ class EmbeddingExtractor:
 
 			# DEPRECATED - NOW THAT CACHE IS STORED BY CATEGORY
 			# df["category"] = [os.path.basename(os.path.dirname(path)) for path in image_paths]
-			
+
 			if output_path is not None:
 				# add the image names to the CSV file
 				encs.to_csv(output_path)
@@ -79,7 +83,7 @@ class EmbeddingExtractor:
 
 	def make_embedding_path(self, d):
 		return "embeddings/{}_{}_{}_{}.csv".format(
-			os.path.basename(os.path.dirname(d)), 
+			os.path.basename(os.path.dirname(d)),
 			os.path.basename(d),
 			self.model_name,
 			self.make_param_path()
@@ -90,31 +94,34 @@ class EmbeddingExtractor:
 
 
 class GPTExtractor(EmbeddingExtractor):
-	MODELS={"l":(1536,16,48),"m":(1024,8,36),"s":(512,8,24)}
+	MODELS = {"l": (1536, 16, 48), "m": (1024, 8, 36), "s": (512, 8, 24)}
 
 	def __init__(self, model_name, model_size, models_dir, color_clusters_dir, n_px, from_cache=True):
-		super().__init__(model_name)
+		super().__init__(model_name, from_cache)
 
 		self.n_px = n_px
 		self.model_size = model_size
 		self.from_cache = from_cache
 
-		color_clusters_file = "%s/kmeans_centers.npy"%(color_clusters_dir)
-		self.clusters = np.load(color_clusters_file) # get color clusters
-		
-		n_embd, n_head, n_layer = GPTExtractor.MODELS[model_size] # set model hyperparameters
+		color_clusters_file = "%s/kmeans_centers.npy" % color_clusters_dir
+		self.clusters = np.load(color_clusters_file)  # get color clusters
 
-		self.vocab_size = len(self.clusters) + 1 # add one for start of sentence token
+		n_embd, n_head, n_layer = GPTExtractor.MODELS[model_size]  # set model hyperparameters
+
+		self.vocab_size = len(self.clusters) + 1  # add one for start of sentence token
 
 		self.config = transformers.GPT2Config(
 			vocab_size=self.vocab_size,
-			n_ctx=self.n_px*self.n_px,
-			n_positions=self.n_px*self.n_px,
+			n_ctx=self.n_px * self.n_px,
+			n_positions=self.n_px * self.n_px,
 			n_embd=n_embd,
 			n_layer=n_layer,
 			n_head=n_head
 		)
-		self.model_path = "%s/%s/model.ckpt-1000000.index"%(models_dir, model_size)
+		self.model_path = "%s/%s/model.ckpt-1000000.index" % (models_dir, model_size)
+
+	def _extract_context(self, samples, gpu, **extract_kwargs):
+		raise NotImplementedError
 
 	def load_model(self):
 		assert os.path.exists(self.model_path), f"There is no file at {self.model_path}"
@@ -123,21 +130,23 @@ class GPTExtractor(EmbeddingExtractor):
 		)
 
 	def process_samples(self, image_paths, visualize=False):
-		for path in image_paths: assert os.path.exists(path), "ERR: %s is not a valid path." % path
+		for path in image_paths:
+			assert os.path.exists(path), "ERR: %s is not a valid path." % path
 		# print("Num paths: %s" % len(image_paths))
 		x = resize(self.n_px, image_paths)
 		# print("X shape: ", x.shape)
-		x_norm = normalize_img(x) #normalize pixels values to -1 to +1
-		samples = color_quantize_np(x_norm, self.clusters).reshape(x_norm.shape[:-1]) #map pixels to closest color cluster
-		
+		x_norm = normalize_img(x)  # normalize pixels values to -1 to +1
+		samples = color_quantize_np(x_norm, self.clusters).reshape(
+			x_norm.shape[:-1])  # map pixels to closest color cluster
+
 		if visualize:
 			print(os.path.basename(os.path.dirname(image_paths[0])))
 			samples_img = [
 				np.reshape(
 					np.rint(127.5 * (self.clusters[s] + 1.0)), [self.n_px, self.n_px, 3]
 				).astype(np.uint8) for s in samples
-			] # convert color clusters back to pixels
-			f, axes = plt.subplots(1,len(image_paths),dpi=300)
+			]  # convert color clusters back to pixels
+			f, axes = plt.subplots(1, len(image_paths), dpi=300)
 			for img, ax in zip(samples_img, axes):
 				ax.axis('off')
 				ax.imshow(img)
@@ -152,15 +161,15 @@ class GPTExtractor(EmbeddingExtractor):
 		)
 
 	def model_output(self, samples, gpu):
-		context = np.concatenate( 
+		context = np.concatenate(
 			(
-				np.full( (samples.shape[0], 1), self.vocab_size - 1 ),
-				samples.reshape(-1, self.n_px*self.n_px),
+				np.full((samples.shape[0], 1), self.vocab_size - 1),
+				samples.reshape(-1, self.n_px * self.n_px),
 			), axis=1
 		)
-		
+
 		# must drop the last pixel to make room for the SOS
-		context = torch.tensor(context[:,:-1]) if not gpu else torch.tensor(context[:,:-1]).cuda()
+		context = torch.tensor(context[:, :-1]) if not gpu else torch.tensor(context[:, :-1]).cuda()
 		return self.model(context, output_hidden_states=True, return_dict=True)
 
 
@@ -168,9 +177,10 @@ class LogitExtractor(GPTExtractor):
 	def _extract_context(self, samples, gpu, **extract_kwargs):
 		output = self.model_output(samples, gpu)
 		# just use the logit layer
-		enc_last = output.logits[:, -1, :].numpy() if not gpu else enc[:, -1, :].cpu().numpy()  # extract the rep of the last input, as in sent-bias
+		# extract the rep of the last input, as in sent-bias
+		enc_last = output.logits[:, -1, :].numpy()
 
-		return enc_last
+		return enc_last.numpy() if not gpu else enc_last.cpu().numpy()
 
 
 class SENTExtractor(GPTExtractor):
@@ -183,34 +193,45 @@ class SENTExtractor(GPTExtractor):
 		# initialize with SOS token
 		output = self.model_output(samples, gpu)
 
-		enc_last = output.hidden_states[-1][:, -1, :].numpy() if not gpu else enc[:, -1, :].cpu().numpy()  # extract the rep of the last input
+		enc_last = output.hidden_states[-1][:, -1, :] # extract the rep of the last input
 
-		return enc_last
+		return enc_last.numpy() if not gpu else enc_last.cpu().numpy()
 
 
 class OpenAIExtractor(GPTExtractor):
+	"""
+	OpenAI strategy:
+	1. find $n^l = layer\_norm(h^l)$
+	2. average pool across the sequence dimension:
+	$$ f^l = \langle n^l_i \rangle_i $$
+	"""
 	def _extract_context(self, samples, gpu, **extract_kwargs):
 		l = extract_kwargs.get("l", 20)
 
 		output = self.model_output(samples, gpu)
 
-		enc_last = output.hidden_states[l][:, -1, :].numpy() if not gpu else enc[:, -1, :].cpu().numpy()  # extract the rep of the lth input
+		# extract the rep of the lth input
+		h_l = output.hidden_states[l]
+		norm = self.model.transformer.h[l+1].ln_1(h_l)
+		enc = tf.reduce_mean(norm, axis=1)
 
-		return enc_last
+		return enc.numpy() if not gpu else enc.cpu().numpy()
 
 
 class ln_mod(nn.Module):
-	def __init__(self, nx,eps=1e-5):
+	def __init__(self, nx, eps=1e-5):
 		super().__init__()
 		self.eps = eps
 		self.weight = Parameter(torch.Tensor(nx))
 
-	def forward(self,x): #input is not mean centered
-		return x / torch.sqrt( torch.std(x, axis=-1, unbiased=False, keepdim=True)**2 + self.eps ) * self.weight.data[...,:]        
+	def forward(self, x):  # input is not mean centered
+		return x \
+			/ torch.sqrt(torch.std(x, axis=-1, unbiased=False, keepdim=True) ** 2 + self.eps) \
+			* self.weight.data[..., :]
 
 
 def load_tf_weights_in_image_gpt2(model, config, gpt2_checkpoint_path):
-	""" 
+	"""
 	Load tf checkpoints in a pytorch model
 	"""
 	try:
@@ -242,16 +263,16 @@ def load_tf_weights_in_image_gpt2(model, config, gpt2_checkpoint_path):
 		# adam_v and adam_m are variables used in AdamWeightDecayOptimizer to calculated m and v
 		# which are not required for using pretrained model
 		if any(
-			n in ["adam_v", "adam_m", "AdamWeightDecayOptimizer", "AdamWeightDecayOptimizer_1", "global_step"]
-			for n in name
+				n in ["adam_v", "adam_m", "AdamWeightDecayOptimizer", "AdamWeightDecayOptimizer_1", "global_step"]
+				for n in name
 		) or name[-1] in ['_step']:
 			logger.debug("Skipping {}".format("/".join(name)))
 			continue
-		
+
 		pointer = model
 		if name[-1] not in ["wtet"]:
-		  pointer = getattr(pointer, "transformer")
-		
+			pointer = getattr(pointer, "transformer")
+
 		for m_name in name:
 			if re.fullmatch(r"[A-Za-z]+\d+", m_name):
 				scope_names = re.split(r"(\d+)", m_name)
@@ -265,17 +286,17 @@ def load_tf_weights_in_image_gpt2(model, config, gpt2_checkpoint_path):
 			elif scope_names[0] == "wpe" or scope_names[0] == "wte":
 				pointer = getattr(pointer, scope_names[0])
 				pointer = getattr(pointer, "weight")
-			elif scope_names[0] in ['q_proj','k_proj','v_proj']:
+			elif scope_names[0] in ['q_proj', 'k_proj', 'v_proj']:
 				pointer = getattr(pointer, 'c_attn')
 				pointer = getattr(pointer, 'weight')
-			elif len(name) ==3 and name[1]=="attn" and scope_names[0]=="c_proj":
+			elif len(name) == 3 and name[1] == "attn" and scope_names[0] == "c_proj":
 				pointer = getattr(pointer, scope_names[0])
 				pointer = getattr(pointer, 'weight')
-			elif scope_names[0]=="wtet":
+			elif scope_names[0] == "wtet":
 				pointer = getattr(pointer, "lm_head")
 				pointer = getattr(pointer, 'weight')
-			elif scope_names[0]=="sos":
-				pointer = getattr(pointer,"wte")
+			elif scope_names[0] == "sos":
+				pointer = getattr(pointer, "wte")
 				pointer = getattr(pointer, 'weight')
 			else:
 				pointer = getattr(pointer, scope_names[0])
@@ -283,33 +304,34 @@ def load_tf_weights_in_image_gpt2(model, config, gpt2_checkpoint_path):
 				num = int(scope_names[1])
 				pointer = pointer[num]
 
-		if len(name) > 1 and name[1]=="attn" or name[-1]=="wtet" or name[-1]=="sos" or name[-1]=="wte":
-		   pass #array is used to initialize only part of the pointer so sizes won't match
+		if len(name) > 1 and name[1] == "attn" or name[-1] == "wtet" or name[-1] == "sos" or name[-1] == "wte":
+			pass  # array is used to initialize only part of the pointer so sizes won't match
 		else:
-		  try:
-			  assert pointer.shape == array.shape
-		  except AssertionError as e:
-			  e.args += (pointer.shape, array.shape)
-			  raise
-		  
+			try:
+				assert pointer.shape == array.shape
+			except AssertionError as e:
+				e.args += (pointer.shape, array.shape)
+				raise
+
 		logger.debug("Initialize PyTorch weight {}".format(name))
 
-		if name[-1]=="q_proj":
-		  pointer.data[:,:config.n_embd] = torch.from_numpy(array.reshape(config.n_embd,config.n_embd) ).T
-		elif name[-1]=="k_proj":
-		  pointer.data[:,config.n_embd:2*config.n_embd] = torch.from_numpy(array.reshape(config.n_embd,config.n_embd) ).T
-		elif name[-1]=="v_proj":
-		  pointer.data[:,2*config.n_embd:] = torch.from_numpy(array.reshape(config.n_embd,config.n_embd) ).T
-		elif (len(name) ==3 and name[1]=="attn" and name[2]=="c_proj" ):
-		  pointer.data = torch.from_numpy(array.reshape(config.n_embd,config.n_embd) )
-		elif name[-1]=="wtet":
-		  pointer.data = torch.from_numpy(array)
-		elif name[-1]=="wte":
-		  pointer.data[:config.vocab_size-1,:] = torch.from_numpy(array)
-		elif name[-1]=="sos":
-		  pointer.data[-1] = torch.from_numpy(array)
+		if name[-1] == "q_proj":
+			pointer.data[:, :config.n_embd] = torch.from_numpy(array.reshape(config.n_embd, config.n_embd)).T
+		elif name[-1] == "k_proj":
+			pointer.data[:, config.n_embd:2 * config.n_embd] = torch.from_numpy(
+				array.reshape(config.n_embd, config.n_embd)).T
+		elif name[-1] == "v_proj":
+			pointer.data[:, 2 * config.n_embd:] = torch.from_numpy(array.reshape(config.n_embd, config.n_embd)).T
+		elif len(name) == 3 and name[1] == "attn" and name[2] == "c_proj":
+			pointer.data = torch.from_numpy(array.reshape(config.n_embd, config.n_embd))
+		elif name[-1] == "wtet":
+			pointer.data = torch.from_numpy(array)
+		elif name[-1] == "wte":
+			pointer.data[:config.vocab_size - 1, :] = torch.from_numpy(array)
+		elif name[-1] == "sos":
+			pointer.data[-1] = torch.from_numpy(array)
 		else:
-		  pointer.data = torch.from_numpy(array)
+			pointer.data = torch.from_numpy(array)
 
 	return model
 
@@ -318,10 +340,10 @@ def replace_ln(m, name, config):
 	for attr_str in dir(m):
 		target_attr = getattr(m, attr_str)
 		if type(target_attr) == torch.nn.LayerNorm:
-			setattr(m, attr_str, ln_mod(config.n_embd,config.layer_norm_epsilon))
+			setattr(m, attr_str, ln_mod(config.n_embd, config.layer_norm_epsilon))
 
 	for n, ch in m.named_children():
-		replace_ln(ch, n,config) 
+		replace_ln(ch, n, config)
 
 
 class ImageGPT2LMHeadModel(GPT2LMHeadModel):
@@ -330,12 +352,12 @@ class ImageGPT2LMHeadModel(GPT2LMHeadModel):
 	def __init__(self, config):
 		super().__init__(config)
 		self.lm_head = nn.Linear(config.n_embd, config.vocab_size - 1, bias=False)
-		replace_ln(self, "net",config) #replace layer normalization
+		replace_ln(self, "net", config)  # replace layer normalization
 		for n in range(config.n_layer):
-			self.transformer.h[n].mlp.act = ImageGPT2LMHeadModel.gelu2 #replace activation 
+			self.transformer.h[n].mlp.act = ImageGPT2LMHeadModel.gelu2  # replace activation
 
-	def tie_weights(self): #image-gpt doesn't tie output and input embeddings
-		pass 
+	def tie_weights(self):  # image-gpt doesn't tie output and input embeddings
+		pass
 
 	@staticmethod
 	def gelu2(x):
